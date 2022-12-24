5d854dab9142fbe4fbf3de3121da026995c54d3b
natalyatum <natalyamastylo25@gmail.com>
2022-12-24 17:13:45 +0200

    -+*

--- HW2/task3HW2.py
+++ HW2/task3HW2.py
@@ -1,8 +1,13 @@
 #variables a and b
 a = 5
 b = 10
-#Addition1
+#Addition Subtraction
 print(sum(a , b))
-
+print(b - a)
+#Division Multiplication
+print(a * b)
+print(b / a)
+#Exponent (Power)
+print(a ** 2)
 
 
