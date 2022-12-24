f633504f7d162761656abc598a9a0a810e71d895
natalyatum <natalyamastylo25@gmail.com>
2022-12-24 15:22:11 +0200

    name+day

--- HW2/task1HW2.py
+++ HW2/task1HW2.py
@@ -1,6 +1,11 @@
+#example1
 #name
 print(my_name(Natalya))
 #day
 print(my_name_day(Wednesday))
 #конкатенація
 print("Good day"+my_name + "!"+my_name_day + "is a perfect day to learn some python.")
+#example2
+name = "Natalya"
+day = "Wednesday"
+print(f"Good day {name} ! {day} is a perfect day to learn some python.")
