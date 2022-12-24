db969af10e23ce90e9abb5af622acd0f456dbfac
natalyatum <natalyamastylo25@gmail.com>
2022-12-24 13:08:41 +0200

    letter O, H

--- HW1/task2.py
+++ HW1/task2.py
@@ -1,6 +1,16 @@
+#letter - O
 print(
     "#"*9 ,
     "#\t#" ,
     sep="\n" ,
     end="\n"
+)
+#letter - H
+print(
+    "#\t#" ,
+    "#\t#" ,
+    "#"*9,
+    "#\t#",
+    "#\t#" ,
+   sep="\n"  
 )
