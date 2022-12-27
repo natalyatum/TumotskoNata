110768e218e423da2a9d96511ec63d039be8a984
natalyatum <natalyamastylo25@gmail.com>
2022-12-27 17:10:48 +0200

    a quiz

---
+++ HW3/task3.py
@@ -0,0 +1,12 @@
+#math quiz
+#General questions
+name = input("What is your name?")
+age = input("How old are you?")
+#+,-,*,**,/calculator
+What =input("What are we doing? (+, -): " )
+a = input("Enter the first number")
+b = input("Enter the second number")
+if What == "+":
+     c = a + b
+if What == "-":
+    c = a - b
