07d0c799600e5c4f6ef7c590ed5bb07ae2aec3b2
natalyatum <natalyamastylo25@gmail.com>
2022-12-29 14:39:46 +0200

    List comprehension exercise

---
+++ HW6/task3.py
@@ -0,0 +1,8 @@
+#List comprehension exercise
+#Use a list comprehension to make a list containing tuples (i, j) where `i` goes from 1 to 10 and `j` is corresponding to `i` squared.
+h_letters = []
+
+for letter in 'human':
+    h_letters.append(letter)
+
+print(h_letters)
