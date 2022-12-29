09fde2aff050e09a1b1744d274a5b9986894ea49
natalyatum <natalyamastylo25@gmail.com>
2022-12-29 13:48:52 +0200

    phone number

---
+++ HW3/task2.py
@@ -0,0 +1,18 @@
+
+# Initialising string
+ini_string1 = '1234556'
+ini_string2 = 'ab123bc'
+ 
+# printing initial string
+print("Initial Strings : ", ini_string1, ini_string2)
+ 
+# Using isdigit()
+if ini_string1.isdigit():
+    print("String1 contains all numbers")
+else:
+    print("String1 doesn't contains all numbers")
+ 
+if ini_string2.isdigit():
+    print("String2 contains all numbers")
+else:
+    print("String2 doesn't contains all numbers")
